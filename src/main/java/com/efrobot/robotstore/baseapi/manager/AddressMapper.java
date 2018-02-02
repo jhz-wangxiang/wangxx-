c1a8afa7af553dda8969a4fc71733b8d2f60ef3c
wangxiang <wangxiangxiang@ren001.com>
2018-02-02 11:23:13 +0800

    "air"

--- src/main/java/com/efrobot/robotstore/baseapi/manager/AddressMapper.java
+++ src/main/java/com/efrobot/robotstore/baseapi/manager/AddressMapper.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.efrobot.robotstore.baseapi.manager.pojo.Address;
 
 public interface AddressMapper {
-
+ 
     int deleteByPrimaryKey(Integer id);
 
     int insert(Address record);
