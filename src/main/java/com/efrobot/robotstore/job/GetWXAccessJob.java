48f20f47e956482fc4a1d57cd9397f74f4b92bc4
wangxiang <wangxiangxiang@ren001.com>
2018-02-08 11:39:25 +0800

    "air"

---
+++ src/main/java/com/efrobot/robotstore/job/GetWXAccessJob.java
@@ -0,0 +1,25 @@
+package com.efrobot.robotstore.job;
+
+/**
+ * 
+ * 类的描述:获取微信token,js ticket
+ * Copyright 2015 JIQIREN Inc. All Rights Reserved.
+ * @author wurui. Email:wurui@ren001.com
+ * @date 2015年12月4日 下午2:40:35
+ */
+
+public class GetWXAccessJob {
+
+	
+	protected void execute(){
+		try {
+          System.out.println("job+++++++++++++++");
+			
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
