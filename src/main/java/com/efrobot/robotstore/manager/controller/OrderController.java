274eedf6f1b6e1d5905c849d0631d3620c2bfac2
wangxiang <wangxiangxiang@ren001.com>
2018-02-08 11:31:55 +0800

    "air"

--- src/main/java/com/efrobot/robotstore/manager/controller/OrderController.java
+++ src/main/java/com/efrobot/robotstore/manager/controller/OrderController.java
@@ -37,28 +37,27 @@ import com.efrobot.robotstore.util.Const;
 import com.efrobot.robotstore.util.PageInfo;
 import com.efrobot.robotstore.util.SerialNum;
 
-
 @RequestMapping("/v1/order")
 @RestController
 public class OrderController {
 
 	@Autowired
 	private OrderService orderService;
-	
+
 	@Autowired
 	private FlightNumService flightNumService;
 	@Autowired
 	private UserService userService;
-	public static Map<Integer, String> status_order = new ConcurrentHashMap<Integer,String>();
-	
+	public static Map<Integer, String> status_order = new ConcurrentHashMap<Integer, String>();
+
 	@PostConstruct
 	public void init() {
-		List<OrderStatus> list=orderService.selectSelectList(null);
-		for(OrderStatus s: list){
+		List<OrderStatus> list = orderService.selectSelectList(null);
+		for (OrderStatus s : list) {
 			status_order.put(s.getStatus(), s.getDescribe());
 		}
 	}
-	
+
 	@SuppressWarnings("static-access")
 	@RequestMapping(value = "/getOrderListPage")
 	@ResponseBody
@@ -74,7 +73,7 @@ public class OrderController {
 		jsonObject = JSONObject.parseObject(result);
 		return jsonObject;
 	}
-	
+
 	@SuppressWarnings("static-access")
 	@RequestMapping(value = "/getPrintListPage")
 	@ResponseBody
@@ -83,156 +82,140 @@ public class OrderController {
 		PageInfo<Order> rows = null;
 		JSONObject obj = new JSONObject();
 		String result = "";
-		Order record=new Order();
+		Order record = new Order();
 		record.setList(Arrays.asList(ids.split(",")));
 		rows = orderService.getOrderListPage(record, pageNumber, pageSize);
 		result = obj.toJSONString(rows, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullNumberAsZero,
 				SerializerFeature.WriteNullStringAsEmpty);
-		
+
 		jsonObject = JSONObject.parseObject(result);
 		return jsonObject;
 	}
-	
+
 	@RequestMapping(value = "/getOrderDetail", method = RequestMethod.POST)
 	@ResponseBody
 	public Order getOrderDetail(Order record) throws Exception {
-		List<Order> orderList= orderService.selectByParms(record);
+		List<Order> orderList = orderService.selectByParms(record);
 		return orderList.get(0);
 	}
-	
+
 	@RequestMapping(value = "/insertOrder", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> insertOrder(Order record) throws Exception {
 		int result = -1;
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 		SimpleDateFormat mmdd = new SimpleDateFormat("MMdd");
-		String datestr=sdf.format(new Date());
-		if("".equals(record.getNowTimeStr())||null==record.getNowTimeStr()){
+		String datestr = sdf.format(new Date());
+		if ("".equals(record.getNowTimeStr()) || null == record.getNowTimeStr()) {
 			return CommonUtil.resultMsg("FAIL", "航班日期不能为空 ");
 		}
-		if("".equals(record.getFlightNum())||null==record.getFlightNum()){
+		if ("".equals(record.getFlightNum()) || null == record.getFlightNum()) {
 			return CommonUtil.resultMsg("FAIL", "航班日期不能为空 ");
 		}
-		FlightNum f=new FlightNum();
+		FlightNum f = new FlightNum();
 		f.setFlightNum(record.getFlightNum());
-		List<FlightNum> list=flightNumService.selectByParms(f);
-		if(list.size()==0){
+		List<FlightNum> list = flightNumService.selectByParms(f);
+		if (list.size() == 0) {
 			return CommonUtil.resultMsg("FAIL", "此航班无法托运行李 ");
 		}
-		FlightNum flightNum =list.get(0);
-		//校验航班时间
-        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
-		
-		String time=sdf.format(record.getNowTime())+" "+flightNum.getEndHour()+":00:00";
-		
-		try {
-			Date t=sdf2.parse(time);
-			Date n=new Date();
-//			long  between = t.getTime() - n.getTime();
-			if(!jisuan(n,t)){
-				return CommonUtil.resultMsg("FAIL", "航班未开放，请于落地前 24 小时内下单");//订单可在航班计划落地时间前 24 小时内下单，如早下单，则提示
-			}
-//			if(between < (24* 3600000)||between>0){
-//			}else{
-//				return CommonUtil.resultMsg("FAIL", "航班未开放，请于落地前 24 小时内下单");//订单可在航班计划落地时间前 24 小时内下单，如早下单，则提示
-//				//航班未开放，请于落地前 24 小时内下单。未支付订单保留至航班计划落地时间
-//				//后 24 小时，将自动取消。未确认订单在航班计划落地时间后 24 小时，将自动
-//				//退款，并标识订单取消。（
-//			}
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		FlightNum flightNum = list.get(0);
+		// 校验航班时间
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+
+		String time = sdf.format(record.getNowTime()) + " " + flightNum.getEndHour() + ":00:00";
+
+		Date t = sdf2.parse(time);
+		Date n = new Date();
+		if (!jisuan(n, t)) {
+			return CommonUtil.resultMsg("FAIL", "航班未开放，请于落地前 24 小时内下单");// 订单可在航班计划落地时间前
+																		// 24
+																		// 小时内下单，如早下单，则提示
 		}
-		String zm=flightNum.getExp1();
-		String orderNo=zm+datestr+mmdd.format(record.getNowTime())+record.getFlightNum()+SerialNum.getSystemManageOrder();
+		String zm = flightNum.getExp1();
+		String orderNo = zm + datestr + mmdd.format(record.getNowTime()) + record.getFlightNum()
+				+ SerialNum.getSystemManageOrder();
 		Subject subject = SecurityUtils.getSubject();
 		Session session = subject.getSession();
-		SysUser sysUser=(SysUser) session.getAttribute(Const.SESSION_USER);
+		SysUser sysUser = (SysUser) session.getAttribute(Const.SESSION_USER);
 		// 异常处理
-		record.setOrderStatus(1);//支付变成2
+		record.setOrderStatus(1);// 支付变成2
 		record.setPayStatus("未支付");
 		record.setCreateDate(new Date());
 		record.setOrderNo(orderNo);
 		record.setSingleWay("柜台");
 		record.setAbnormalStatus("否");
-		record.setOperator("柜台"+sysUser.getName());
+		record.setOperator("柜台" + sysUser.getName());
 		result = orderService.insertSelective(record);
 		if (result == 0) {
 			return CommonUtil.resultMsg("FAIL", "未找到可编辑的信息");
-		} else if (result == 1){
-			User user=new User();
+		} else if (result == 1) {
+			User user = new User();
 			user.setOrderDate(new Date());
 			user.setExp2("有");
 			user.setId(record.getUserId());
 			userService.updateByPrimaryKeySelective(user);
-			setHistory("提交订单",orderNo,"");
+			setHistory("提交订单", orderNo, "");
 			return CommonUtil.resultMsg("SUCCESS", "信息插入功");
-		}else {
+		} else {
 			return CommonUtil.resultMsg("FAIL", "更新异常: 多条数据被更新 ");
 		}
 	}
-	
+
 	@RequestMapping(value = "/insertOrderbyUser", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> insertOrderbyUser(Order record) throws Exception {
 		int result = -1;
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 		SimpleDateFormat mmdd = new SimpleDateFormat("MMdd");
-		String datestr=sdf.format(new Date());
-		if("".equals(record.getNowTimeStr())||null==record.getNowTimeStr()){
+		String datestr = sdf.format(new Date());
+		if ("".equals(record.getNowTimeStr()) || null == record.getNowTimeStr()) {
 			return CommonUtil.resultMsg("FAIL", "航班日期不能为空 ");
 		}
-		if("".equals(record.getFlightNum())||null==record.getFlightNum()){
+		if ("".equals(record.getFlightNum()) || null == record.getFlightNum()) {
 			return CommonUtil.resultMsg("FAIL", "航班日期不能为空 ");
 		}
-		FlightNum f=new FlightNum();
+		FlightNum f = new FlightNum();
 		f.setFlightNum(record.getFlightNum());
-		List<FlightNum> list=flightNumService.selectByParms(f);
-		if(list.size()==0){
+		List<FlightNum> list = flightNumService.selectByParms(f);
+		if (list.size() == 0) {
 			return CommonUtil.resultMsg("FAIL", "此航班无法托运行李 ");
 		}
-		FlightNum flightNum =list.get(0);
-		//校验航班时间
-        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
-		
-		String time=sdf.format(record.getNowTime())+" "+flightNum.getEndHour()+":00:00";
-		
-		try {
-			Date t=sdf2.parse(time);
-			Date n=new Date();
-//			long  between = t.getTime() - n.getTime();
-			if(!jisuan(n,t)){
-				return CommonUtil.resultMsg("FAIL", "航班未开放，请于落地前 24 小时内下单");//订单可在航班计划落地时间前 24 小时内下单，如早下单，则提示
-			}
-//			if(between < (24* 3600000)||between>0){
-//			}else{
-//				return CommonUtil.resultMsg("FAIL", "航班未开放，请于落地前 24 小时内下单");//订单可在航班计划落地时间前 24 小时内下单，如早下单，则提示
-//				//航班未开放，请于落地前 24 小时内下单。未支付订单保留至航班计划落地时间
-//				//后 24 小时，将自动取消。未确认订单在航班计划落地时间后 24 小时，将自动
-//				//退款，并标识订单取消。（
-//			}
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		FlightNum flightNum = list.get(0);
+		// 校验航班时间
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+
+		String time = sdf.format(record.getNowTime()) + " " + flightNum.getEndHour() + ":00:00";
+
+		Date t = sdf2.parse(time);
+		Date n = new Date();
+		if (!jisuan(n, t)) {
+			return CommonUtil.resultMsg("FAIL", "航班未开放，请于落地前 24 小时内下单");// 订单可在航班计划落地时间前
+																		// 24
+																		// 小时内下单，如早下单，则提示
 		}
-		String zm=flightNum.getExp1();
-		String orderNo=zm+datestr+mmdd.format(record.getNowTime())+record.getFlightNum()+SerialNum.getSystemManageOrder();
+		// 订单可在航班计划落地时间前 24 小时内下单，如早下单，则提示
+		// //航班未开放，请于落地前 24 小时内下单。未支付订单保留至航班计划落地时间
+		// //后 24 小时，将自动取消。未确认订单在航班计划落地时间后 24 小时，将自动
+		// //退款，并标识订单取消。（
+		String zm = flightNum.getExp1();
+		String orderNo = zm + datestr + mmdd.format(record.getNowTime()) + record.getFlightNum()
+				+ SerialNum.getSystemManageOrder();
 		Subject subject = SecurityUtils.getSubject();
 		Session session = subject.getSession();
-		SysUser sysUser=(SysUser) session.getAttribute(Const.SESSION_USER);
+		SysUser sysUser = (SysUser) session.getAttribute(Const.SESSION_USER);
 		// 异常处理
-		record.setOrderStatus(1);//支付变成2
+		record.setOrderStatus(1);// 支付变成2
 		record.setPayStatus("未支付");
 		record.setCreateDate(new Date());
 		record.setOrderNo(orderNo);
 		record.setSingleWay("柜台");
 		record.setAbnormalStatus("否");
-		record.setOperator("柜台"+sysUser.getName());
+		record.setOperator("柜台" + sysUser.getName());
 		result = orderService.insertSelective(record);
 		if (result == 0) {
 			return CommonUtil.resultMsg("FAIL", "未找到可编辑的信息");
-		} else if (result == 1){
-			User user=new User();
+		} else if (result == 1) {
+			User user = new User();
 			user.setOrderDate(new Date());
 			user.setExp2("有");
 			user.setId(record.getUserId());
@@ -240,29 +223,31 @@ public class OrderController {
 			user.setName(record.getName());
 			user.setChannelId(record.getChannelId());
 			userService.updateByPrimaryKeySelective(user);
-			setHistory("提交订单",orderNo,"");
+			setHistory("提交订单", orderNo, "");
 			return CommonUtil.resultMsg("SUCCESS", "信息插入功");
-		}else {
+		} else {
 			return CommonUtil.resultMsg("FAIL", "更新异常: 多条数据被更新 ");
 		}
 	}
-	public static boolean jisuan(Date start, Date end) throws Exception { 
-        long cha = end.getTime() - start.getTime(); 
-        double result = cha * 1.0 / (1000 * 60 * 60); 
-        if(result<=24){ 
-        	if(result>0){
-        		return false; 
-        	}
-             //System.out.println("可用");   
-             return true; 
-        }else{ 
-             //System.out.println("已过期");  
-             return false; 
-        } 
-    } 
+
+	public static boolean jisuan(Date start, Date end) throws Exception {
+		long cha = end.getTime() - start.getTime();
+		double result = cha * 1.0 / (1000 * 60 * 60);
+		if (result <= 24) {
+			if (result > 0) {
+				return false;
+			}
+			// System.out.println("可用");
+			return true;
+		} else {
+			// System.out.println("已过期");
+			return false;
+		}
+	}
+
 	@RequestMapping(value = "/updateOrder", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> updateOrder(Order record ) throws Exception {
+	public Map<String, Object> updateOrder(Order record) throws Exception {
 		int result = -1;
 		result = orderService.updateByPrimaryKeySelective(record);
 		if (result == 0) {
@@ -273,19 +258,20 @@ public class OrderController {
 			return CommonUtil.resultMsg("FAIL", "更新异常: 多条数据被更新 ");
 		}
 	}
-	//修改订单过程状态
+
+	// 修改订单过程状态
 	@RequestMapping(value = "/updateOrderStatus", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> updateOrderStatus(Order record) throws Exception {
 		int result = -1;
-		Order order2= orderService.selectByPrimaryKey(record.getId());
-		if(order2.getOrderStatus()<6){
-			if(order2.getOrderStatus()==1){
+		Order order2 = orderService.selectByPrimaryKey(record.getId());
+		if (order2.getOrderStatus() < 6) {
+			if (order2.getOrderStatus() == 1) {
 				record.setPayStatus("已支付");
 			}
-			record.setOrderStatus(order2.getOrderStatus()+1);
-			setHistory(status_order.get(order2.getOrderStatus()+1),order2.getOrderNo(),record.getRemark());
-		}else{
+			record.setOrderStatus(order2.getOrderStatus() + 1);
+			setHistory(status_order.get(order2.getOrderStatus() + 1), order2.getOrderNo(), record.getRemark());
+		} else {
 			return CommonUtil.resultMsg("FAIL", "已经签收完结");
 		}
 		result = orderService.updateByPrimaryKeySelective(record);
@@ -297,16 +283,16 @@ public class OrderController {
 			return CommonUtil.resultMsg("FAIL", "更新异常: 多条数据被更新 ");
 		}
 	}
-	
-	//修改订单过程状态
+
+	// 修改订单过程状态
 	@RequestMapping(value = "/updateOrderStop", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> updateOrderStop(Order record) throws Exception {
 		int result = -1;
-		Order order2= orderService.selectByPrimaryKey(record.getId());
+		Order order2 = orderService.selectByPrimaryKey(record.getId());
 		record.setOrderStatus(11);
 		result = orderService.updateByPrimaryKeySelective(record);
-		setHistory(status_order.get(11),order2.getOrderNo(),record.getRemark());
+		setHistory(status_order.get(11), order2.getOrderNo(), record.getRemark());
 		if (result == 0) {
 			return CommonUtil.resultMsg("FAIL", "未找到可编辑的信息");
 		} else if (result == 1)
@@ -315,18 +301,18 @@ public class OrderController {
 			return CommonUtil.resultMsg("FAIL", "更新异常: 多条数据被更新 ");
 		}
 	}
-	
-	//修改取消过程状态
+
+	// 修改取消过程状态
 	@RequestMapping(value = "/updateOrderCancel", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> updateOrderCancel(Order record) throws Exception {
 		int result = -1;
-		Order order2= orderService.selectByPrimaryKey(record.getId());
-		if(order2.getOrderStatus()<2){
+		Order order2 = orderService.selectByPrimaryKey(record.getId());
+		if (order2.getOrderStatus() < 2) {
 			record.setOrderStatus(10);
 			result = orderService.updateByPrimaryKeySelective(record);
-			setHistory(status_order.get("订单取消"),order2.getOrderNo(),record.getCancelReason());
-		}else{
+			setHistory(status_order.get("订单取消"), order2.getOrderNo(), record.getCancelReason());
+		} else {
 			return CommonUtil.resultMsg("FAIL", "现在的状态不可以取消订单");
 		}
 		if (result == 0) {
@@ -337,18 +323,19 @@ public class OrderController {
 			return CommonUtil.resultMsg("FAIL", "更新异常: 多条数据被更新 ");
 		}
 	}
-	//修改取消过程状态
+
+	// 修改取消过程状态
 	@RequestMapping(value = "/updateAbnormalStatus", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> updateAbnormalStatus(Order record) throws Exception {
 		int result = -1;
-		Order order2= orderService.selectByPrimaryKey(record.getId());
-		if("是".equals(order2.getAbnormalStatus())){
+		Order order2 = orderService.selectByPrimaryKey(record.getId());
+		if ("是".equals(order2.getAbnormalStatus())) {
 			record.setAbnormalStatus("否");
-			setHistory("订单异常",order2.getOrderNo(),record.getAbnormaReason());
-		}else{
+			setHistory("订单异常", order2.getOrderNo(), record.getAbnormaReason());
+		} else {
 			record.setAbnormalStatus("是");
-			setHistory("订单正常",order2.getOrderNo(),record.getAbnormaReason());
+			setHistory("订单正常", order2.getOrderNo(), record.getAbnormaReason());
 		}
 		result = orderService.updateByPrimaryKeySelective(record);
 		if (result == 0) {
@@ -359,35 +346,37 @@ public class OrderController {
 			return CommonUtil.resultMsg("FAIL", "更新异常: 多条数据被更新 ");
 		}
 	}
-	
+
 	@RequestMapping(value = "/getOrderStatus", method = RequestMethod.POST)
 	@ResponseBody
 	public List<OrderStatus> getOrderStatus(String ids) throws Exception {
-		  List<OrderStatus> list=orderService.selectSelectList(ids);
-		  return list;
+		List<OrderStatus> list = orderService.selectSelectList(ids);
+		return list;
 	}
-	
+
 	@RequestMapping(value = "/getChannel", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Channel> getChannel(Channel record) throws Exception {
 		record.setExp1("1");
-		List<Channel> list=orderService.getChannel(record);
+		List<Channel> list = orderService.getChannel(record);
 		return list;
 	}
+
 	@RequestMapping(value = "/getArea", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Area> getArea(Area record) throws Exception {
-		List<Area> list=orderService.getArea(record);
+		List<Area> list = orderService.getArea(record);
 		return list;
 	}
-	
+
 	@RequestMapping(value = "/getFlightNum", method = RequestMethod.POST)
 	@ResponseBody
 	public List<FlightNum> getFlightNum(FlightNum record) throws Exception {
-		List<FlightNum> list=orderService.getFlightNum(record);
+		List<FlightNum> list = orderService.getFlightNum(record);
 		return list;
 	}
-	//查询历史状态
+
+	// 查询历史状态
 	@SuppressWarnings("static-access")
 	@RequestMapping(value = "/selectHistory", method = RequestMethod.POST)
 	@ResponseBody
@@ -402,37 +391,39 @@ public class OrderController {
 
 		jsonObject = JSONObject.parseObject(result);
 		return jsonObject;
-//		return orderService.selectByparms(orderNo);
+		// return orderService.selectByparms(orderNo);
 	}
-	public int setHistory(String remark,String orderNo,String reason){
+
+	public int setHistory(String remark, String orderNo, String reason) {
 		Subject subject = SecurityUtils.getSubject();
 		Session session = subject.getSession();
-		SysUser sysUser=(SysUser) session.getAttribute(Const.SESSION_USER);
-		OrderStatusRecord orderStatusRecord=new OrderStatusRecord();
+		SysUser sysUser = (SysUser) session.getAttribute(Const.SESSION_USER);
+		OrderStatusRecord orderStatusRecord = new OrderStatusRecord();
 		orderStatusRecord.setRemark(remark);
 		orderStatusRecord.setExp1(orderNo);
 		orderStatusRecord.setExp2(reason);
 		orderStatusRecord.setRoleId(sysUser.getRoleId());
 		orderStatusRecord.setUserId(sysUser.getId());
 		orderStatusRecord.setCreateDate(new Date());
-		orderStatusRecord.setStatus(1);//1-后台,2-客户
-		int result =orderService.insertSelective(orderStatusRecord);
+		orderStatusRecord.setStatus(1);// 1-后台,2-客户
+		int result = orderService.insertSelective(orderStatusRecord);
 		return result;
-		
+
 	}
+
 	public static void main(String[] args) {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-		
+
 		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd HH");
-		
-		String time=sdf2.format(new Date())+" "+"10";
-		
+
+		String time = sdf2.format(new Date()) + " " + "10";
+
 		try {
-			Date t=sdf2.parse(time);
-			Date n=new Date();
-			long  between = t.getTime() - n.getTime();
-			
-			if(between < (24* 3600000)||between>0){
+			Date t = sdf2.parse(time);
+			Date n = new Date();
+			long between = t.getTime() - n.getTime();
+
+			if (between < (24 * 3600000) || between > 0) {
 				System.out.print(sdf2.format(new Date()));
 			}
 		} catch (ParseException e) {
@@ -440,7 +431,5 @@ public class OrderController {
 			e.printStackTrace();
 		}
 
-		
-		
 	}
 }
